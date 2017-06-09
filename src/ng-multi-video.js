b7e9b5e57996db8b869d329c4b8af4662395c703
Washington Soares <washingtonudi@gmail.com>
2017-06-08 22:11:01 -0300

    Adaptando diretiva para sincronizar os videos iniciais e finais ao video do curso

--- src/ng-multi-video.js
+++ src/ng-multi-video.js
@@ -1,97 +1,118 @@
-'use strict';
+'use strict'
 
-angular.module('multiVideo',[])
-  .directive('multiVideo', ['$compile', '$sce', '$rootScope', '$interval', '$window', function ($compile,$sce,$rootScope,$interval,$window) {
+angular.module('multiVideo', [])
+  .directive('multiVideo', ['$compile', '$sce', '$rootScope', '$interval', '$window', function ($compile, $sce, $rootScope, $interval, $window) {
+    var templateAnguVideo = '<div anguvideo auto-play="{{autoPlay}}" ng-model="videoToPlay"></div>'
+    var templateClappr = '<clappr src="src"></clappr>'
+    var templateLightBox = '<angular-master-lightbox images="src">' +
+                            '</angular-master-lightbox>'
 
-    var templateAnguVideo = '<div anguvideo ng-model="src"></div>';
-    var templateClappr    = '<clappr src="src"></clappr>';
-    var templateLightBox  = '<angular-master-lightbox images="src">' +
-                            '</angular-master-lightbox>';
+    var switchDirectives = function (val) {
+      var htmlDirective
+      var extension = val.substring(val.length - 4)
 
-    var switchDirectives = function(val){
-        var  htmlDirective;
-        var extension = val.substring(val.length - 4);
+      if (extension === '.mp4') {
+        htmlDirective = templateClappr
+      } else if (extension === '.jpg' || extension === '.png' || extension === '.gif') {
+        htmlDirective = templateLightBox
+      } else {
+        htmlDirective = templateAnguVideo
+      }
 
-        if( extension === ".mp4"){
-          htmlDirective = templateClappr;
-        }
-        else if(extension === ".jpg" || extension === ".png" || extension === ".gif"){
-          htmlDirective = templateLightBox;
-        }
-        else{
-          htmlDirective = templateAnguVideo;
-        }
-
-        return htmlDirective;
+      return htmlDirective
     }
 
     return {
       restrict: 'E',
       transclude: true,
-      scope:{
-        src: '=src',
+      scope: {
+        src: '=',
         clickProgress: '&',
         watchedMinPercentage: '&',
         allowEmmitWatchedMinPercentageEvent: '=',
-        automaticNextVideo: '=automaticNextVideo'
+        automaticNextVideo: '=',
+        opening: '=',
+        ending: '='
       },
-      link: function(scope,element,attrs,ctrl,transclude){
+      link: function (scope, element, attrs, ctrl, transclude) {
+        scope.interval = null
 
-        scope.interval = null;
-        scope.$watch('src', watchScopeSrc(element,scope));
-        var multiVideoWatchedMinPercentage = function(){
-          if(scope.allowEmmitWatchedMinPercentageEvent){
-            scope.watchedMinPercentage();
+        if (scope.opening) {
+          scope.videoToPlay = scope.opening
+          scope.hasPlayedOpening = true
+        } else {
+          scope.videoToPlay = scope.src
+        }
+
+        scope.$watch('src', watchScopeSrc(element, scope))
+        var multiVideoWatchedMinPercentage = function () {
+          if (scope.allowEmmitWatchedMinPercentageEvent) {
+            scope.watchedMinPercentage()
           }
-        };
-        var multiVideoFinish = function(){};
-        scope.$on("clappr:finishVideo", multiVideoFinish);
-        scope.$on("anguvideo:finishVideo", multiVideoFinish);
-        scope.$on("$destroy", clearIntervalProgressBar(scope));
-        scope.$on("clappr:watchedMinPercentage", multiVideoWatchedMinPercentage);
-        scope.$on("anguvideo:watchedMinPercentage", multiVideoWatchedMinPercentage)
-      }
-    };
-
-    function clearIntervalProgressBar(scope){
-      return function(){
-        $interval.cancel(scope.interval);
-        scope.interval = undefined;
-        scope.progress = 0;
-
-
-        var mediaElement = angular.element('video')[0];
-        if(mediaElement){
-          mediaElement.pause();
-          mediaElement.src='';
         }
 
-        return false;
+        var multiVideoFinish = function () {
+          if (scope.videoToPlay === scope.opening) {
+            scope.videoToPlay = scope.src
+            scope.autoPlay = '1'
+          } else if (scope.videoToPlay === scope.src && scope.ending) {
+            scope.videoToPlay = scope.ending
+            scope.autoPlay = '1'
+          }
+          scope.$apply()
+        }
+
+        scope.$on('clappr:finishVideo', multiVideoFinish)
+        scope.$on('anguvideo:finishVideo', multiVideoFinish)
+        scope.$on('$destroy', clearIntervalProgressBar(scope))
+        scope.$on('clappr:watchedMinPercentage', multiVideoWatchedMinPercentage)
+        scope.$on('anguvideo:watchedMinPercentage', multiVideoWatchedMinPercentage)
       }
     }
 
-    function watchScopeSrc(element,scope) {
-        return function(newVal){
-          if (!newVal)
-            return;
+    function clearIntervalProgressBar (scope) {
+      return function () {
+        $interval.cancel(scope.interval)
+        scope.interval = undefined
+        scope.progress = 0
 
-           $(".progress-wrapper").hide();
-           clearIntervalProgressBar(scope)();
-           element.html(switchDirectives(newVal)).show();
-           $compile(element.contents())(scope);
+        var mediaElement = angular.element('video')[0]
+        if (mediaElement) {
+          mediaElement.pause()
+          mediaElement.src = ''
         }
 
-    }
-
-    function incrementCurrentProgress(scope){
-      return function(){
-        scope.progress += 0.5;
-        if(scope.progress >= 100){
-          $rootScope.$broadcast("multiVideo:finishProgressbar");
-          clearIntervalProgressBar(scope)();
-          return false;
-        }
+        return false
       }
     }
 
-  }]);
+    function watchScopeSrc (element, scope) {
+      return function (newVal) {
+        if (!newVal) {
+          return
+        }
+
+        if (!scope.hasPlayedOpening) {
+          scope.videoToPlay = newVal
+        }
+
+					 delete scope.hasPlayedOpening
+
+        $('.progress-wrapper').hide()
+        clearIntervalProgressBar(scope)()
+        element.html(switchDirectives(newVal)).show()
+        $compile(element.contents())(scope)
+      }
+    }
+
+    function incrementCurrentProgress (scope) {
+      return function () {
+        scope.progress += 0.5
+        if (scope.progress >= 100) {
+          $rootScope.$broadcast('multiVideo:finishProgressbar')
+          clearIntervalProgressBar(scope)()
+          return false
+        }
+      }
+    }
+  }])
